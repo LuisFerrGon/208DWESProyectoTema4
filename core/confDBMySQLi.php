b04d801e98e636ec085fc1987d7f859a1b6ec8af
Luis Ferreras <luis.fergon.2@educa.jcyl.es>
2024-11-22 08:47:43 +0100

    22/11 	Configuración entorno explotación

--- core/confDBMySQLi.php
+++ core/confDBMySQLi.php
@@ -1,10 +1,15 @@
 <?php
-//    Datos en clase
+////    Datos en entorno de explotacion
+//    define("SERVIDOR", "localhost:3306");
+//    define("USUARIO", "user208DWESProyectoTema4");
+//    define("CONTRASENA", "paso1234321osap");
+//    define("BASEDATOS", "DB208DWESProyectoTema4");
+//    Datos en entorno desarrollo de clase
     define("SERVIDOR", "daw208.isauces.local");
     define("USUARIO", "user208DWESProyectoTema4");
     define("CONTRASENA", "paso");
     define("BASEDATOS", "DB208DWESProyectoTema4");
-//    Datos en casa
+//    Datos en entorno desarrollo de casa
 //    define("SERVIDOR", "192.168.0.208");
 //    define("USUARIO", "user208DWESProyectoTema4");
 //    define("CONTRASENA", "paso");
